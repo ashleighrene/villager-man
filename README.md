b8fe14866f414bb55ecb996d40f61b972c3a58c4
ashleighrene <arwaller7@gmail.com>
2021-12-08 12:56:24 -0800

    update README.md MVP and Bronze.  Add JS logic for random word, letter selection.

--- README.md
+++ README.md
@@ -11,17 +11,17 @@ A browser-based Spaceman game (with an Animal Crossing twist) coded in JavaScrip
 #### MVP
 
 - As a user, I want a browser-based interface so that I can play the game.
-- As a user, I want to be able to enter a word (between three and eight characters in length) for another player to guess.
+- As a user, I want to be able to enter letters with my keyboard against the randomly chosen word.
 - As a user, I want to see a villager image leave if a letter is guessed incorrectly (start with 10 villagers). There should be a lose state with a reset button.
 - As a user, I want to see a letter appear in the word space if it was guessed correctly.
 - As a user, I want the game to reach a win state so I know that the word is complete and no more letters can be guessed.
-- As a user, I want the other play to be able to guess a letter using their keyboard.
 - As a user, I want incorrect letters to stay on the screen in a container.
 - As a user, I want to have instructions available if someone is not sure how to play.
 
 #### Bronze
 
 - As a user, I want to be able to see how long it takes for the second player to guess the word (timer).
+- As a user, I want to be able to enter a word (between three and eight characters in length) for another player to guess.
 
 #### Silver
 
--- script.js
+++ script.js
@@ -15,3 +15,82 @@ const closeModal = () => {
 
 openBtn.addEventListener('click', openModal);
 close.addEventListener('click', closeModal);
+
+// ______________________________________________
+
+let letterBoard = document.querySelector('#hiddenWord');
+let trashHeap = document.querySelector('#trash');
+let wrongAnswer = [];
+
+// Array for attempted letters that will go in trash
+let lettersAttempted = [];
+
+//Array of villagers.  Loop through array and lose condition if it === 0;
+let villagerArray = document.querySelectorAll('.villagerTop.villagerBottom');
+
+// After checking that word is complete
+let winStates;
+// After all villagers gone
+let checkLoss;
+
+// Array of words with random selection to start. They should all be lower case.
+let wordBank = [
+	'nook',
+	'island',
+	'fishing',
+	'build',
+	'craft',
+	'coffee',
+	'nintendo',
+	'seagull',
+	'villagers',
+	'neighbors',
+	'fireworks',
+	'hammock',
+	'cooking',
+	'baking',
+	'gifts',
+	'scuba',
+];
+
+// Function to randomly choose word from wordBank
+let answerWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+// Show answer word in console
+console.log(answerWord);
+
+// Player Guesses
+let inputField = document.querySelector('#guessInputBox');
+inputField.addEventListener('keydown', addLetter);
+function addLetter(event) {
+	if (event.keyCode >= 66 && event.keyCode <= 91) {
+		let letter = event.key.toLowerCase();
+		//Check to see if letter is in answerWord with .incluces.
+		// .includes returns a boolean.
+		answerWord.includes(letter);
+
+		//True goes into letterBoard
+
+		//False goes into trashHeap
+		wrongAnswer.push(letter);
+		console.log(wrongAnswer);
+
+		trashHeap.innerText = wrongAnswer;
+	}
+}
+
+resetBtn.addEventListener('click', init);
+function init() {}
+
+// If letter is found in word, show on board above underline
+// If letter is not found in word, letter will go to trash heap and one villager will be removed
+// Check to see if word is complete (checkWin)
+// Check to see if villager array is empty (checkLoss)
+// Repeat Player actions until checkWin or checkLoss becomes true
+// When checkWin is true, show win screen and reminder of reset button to play again
+// When checkLoss is true, show loss screen and reminder of reset button to play again
+
+// Reference for keyCode
+// Utilized https://en.wikipedia.org/wiki/List_of_Unicode_characters#Basic_Latin
+
+//Reference for addLetter function
+//https://stackoverflow.com/questions/58469995/javascript-trying-to-allow-a-key-press-only-once-in-hangman-game-when-it-is-de
